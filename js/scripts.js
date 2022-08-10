0cb91fc411d2596948792f25ccf6d11fde4a3d68
Alvaro Alonso <airuritac@gmail.com>
2022-08-10 14:33:33 +0200

    add posts with the API

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,18 @@
 
 
-    // NAV RESPONSIVE
+// NAV RESPONSIVE
 
 window.addEventListener('DOMContentLoaded', () => {
     let scrollPos = 0;
     const mainNav = document.getElementById('mainNav');
     const headerHeight = mainNav.clientHeight;
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
         const currentTop = document.body.getBoundingClientRect().top * -1;
-        if ( currentTop < scrollPos) {
+        if (currentTop < scrollPos) {
             // Scrolling Up
             if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
                 mainNav.classList.add('is-visible');
             } else {
-                console.log(123);
                 mainNav.classList.remove('is-visible', 'is-fixed');
             }
         } else {
@@ -27,5 +26,62 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 })
 
-    // 
+// API
+
+window.onload = fetiche();
+
+const
+    containerPost = document.getElementById("containerPost");
+
+
+
+function fetiche() {
+    for (let i = 0; i < 5; i++) {
+        fetch('http://localhost:3333/posts')
+            .then(data => data.json())
+            .then(posts => {
+
+                let line = document.createElement("hr")
+                line.className = 'my-4';
+
+                let sectionPost = document.createElement("section")
+                sectionPost.className = "displayPost";
+                sectionPost.onclick = () => modalPost(); // Add the function to watch the "MODAL POST"
+
+                let postTittle = document.createElement('h2');
+                postTittle.className = 'post-title';
+                postTittle.innerHTML = posts[i].title;
+
+                let subTitle = document.createElement('h3');
+                subTitle.className = 'subTitle post-subtitle';
+                subTitle.innerHTML = posts[i].body;
+
+                let parraph = document.createElement("p");
+                parraph.className = "post-meta nameParraph";
+                parraph.innerHTML = `Posted by
+                <a id="namePost1" style= "text-decoration: none;" href="#!">Metalligirl</a>
+                on September 24, 2022`;
+                parraph.style.fontSize = "1.125rem";
+                parraph.style.fontStyle = "italic";
+                parraph.style.marginTop = "0";
+                parraph.style.color = "#6c757d";
+                parraph.style.color = "2rem 0";
+
+
+
+                if (i != 0) {
+                    containerPost.appendChild(line);
+                    containerPost.appendChild(sectionPost)
+                    sectionPost.appendChild(postTittle);
+                    sectionPost.appendChild(subTitle);
+                    sectionPost.appendChild(parraph)
+                }else {
+                    containerPost.appendChild(sectionPost)
+                    sectionPost.appendChild(postTittle);
+                    sectionPost.appendChild(subTitle);
+                    sectionPost.appendChild(parraph)
+                }
+            })
+    }
+}
 
