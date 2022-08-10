2b0946a9706e45dffa4008e3bb6a0a3b1e97c705
Alvaro Alonso <airuritac@gmail.com>
2022-08-10 21:06:04 +0200

    Modal Post and organizar everything

---
+++ js/allPost.js
@@ -0,0 +1,81 @@
+
+
+// NAV RESPONSIVE
+
+window.addEventListener('DOMContentLoaded', () => {
+    let scrollPos = 0;
+    const mainNav = document.getElementById('mainNav');
+    const headerHeight = mainNav.clientHeight;
+    window.addEventListener('scroll', function () {
+        const currentTop = document.body.getBoundingClientRect().top * -1;
+        if (currentTop < scrollPos) {
+            // Scrolling Up
+            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-visible');
+            } else {
+                mainNav.classList.remove('is-visible', 'is-fixed');
+            }
+        } else {
+            // Scrolling Down
+            mainNav.classList.remove(['is-visible']);
+            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-fixed');
+            }
+        }
+        scrollPos = currentTop;
+    });
+})
+
+// Load all POSTS
+
+window.onload = feticheAll();
+
+function feticheAll() {
+    for (let a = 0; a < 100; a++) {
+        fetch('http://localhost:3333/posts')
+            .then(data => data.json())
+            .then(posts => {
+
+                let line = document.createElement("hr")
+                line.className = 'hr-divider';
+
+                let sectionPostAll = document.createElement("sectionAll")
+                sectionPostAll.className = "displayPost";
+                sectionPostAll.onclick = () => modalPostAll(); // Add the function to watch the "MODAL POST"
+
+                let postTittle = document.createElement('h2');
+                postTittle.className = 'post-title';
+                postTittle.innerHTML = posts[a].title;
+
+                let subTitle = document.createElement('h3');
+                subTitle.className = 'subTitle post-subtitle';
+                subTitle.innerHTML = posts[a].body;
+
+                let parraph = document.createElement("p");
+                parraph.className = "post-meta nameParraph";
+                parraph.innerHTML = `Posted by
+                <a id="namePost1" style= "text-decoration: none;" href="#!">Metalligirl</a>
+                on September 24, 2022`;
+                parraph.style.fontSize = "1.125rem";
+                parraph.style.fontStyle = "italic";
+                parraph.style.marginTop = "0";
+                parraph.style.color = "#6c757d";
+                parraph.style.color = "2rem 0";
+
+
+
+                if (a != 0) {
+                    containerPostAll.appendChild(line);
+                    containerPostAll.appendChild(sectionPostAll)
+                    sectionPostAll.appendChild(postTittle);
+                    sectionPostAll.appendChild(subTitle);
+                    sectionPostAll.appendChild(parraph)
+                } else {
+                    containerPostAll.appendChild(sectionPostAll)
+                    sectionPostAll.appendChild(postTittle);
+                    sectionPostAll.appendChild(subTitle);
+                    sectionPostAll.appendChild(parraph)
+                }
+            })
+    }
+}
--- js/scripts.js
+++ js/scripts.js
@@ -32,13 +32,13 @@ window.onload = fetiche();
 
 const
     containerPost = document.getElementById("containerPost"),
-    mainDisplay   = document.getElementById("main"),
-    container     = document.getElementById("container");
+    mainDisplay = document.getElementById("main"),
+    container = document.getElementById("container");
 
 
 
 function fetiche() {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 3; i++) {
         fetch('http://localhost:3333/posts')
             .then(info => info.json())
             .then(posts => {
@@ -73,12 +73,12 @@ function fetiche() {
                 let spanIcon = document.createElement("span");
 
                 let iconEdit = document.createElement("img");
-                iconEdit.className ="icons";
+                iconEdit.className = "icons";
                 iconEdit.setAttribute("src", "assets/img/edit.png");
                 iconEdit.onclick = () => modalPost(i); // Edit de object content
 
                 let iconDelete = document.createElement("img");
-                iconDelete.className ="icons";
+                iconDelete.className = "icons";
                 iconDelete.setAttribute("src", "assets/img/delete.png");
                 iconDelete.onclick = () => modalPost(i); // Remove all the post
 
@@ -93,7 +93,7 @@ function fetiche() {
                     spanIcon.appendChild(iconEdit);
                     spanIcon.appendChild(iconDelete);
 
-                }else {
+                } else {
                     containerPost.appendChild(sectionPost)
                     sectionPost.appendChild(postTittle);
                     sectionPost.appendChild(subTitle);
@@ -106,66 +106,108 @@ function fetiche() {
     }
 }
 
-function feticheAll() {
-    for (let a = 0; a < 500; a++) {
-        fetch('http://localhost:3000/posts')
-            .then(data => data.json())
-            .then(posts => {
-
-                let line = document.createElement("hr")
-                line.className = 'hr-divider';
-
-                let sectionPostAll = document.createElement("sectionAll")
-                sectionPostAll.className = "displayPost";
-                sectionPostAll.onclick = () => modalPostAll(); // Add the function to watch the "MODAL POST"
-
-                let postTittle = document.createElement('h2');
-                postTittle.className = 'post-title';
-                postTittle.innerHTML = posts[a].title;
-
-                let subTitle = document.createElement('h3');
-                subTitle.className = 'subTitle post-subtitle';
-                subTitle.innerHTML = posts[a].body;
-
-                let parraph = document.createElement("p");
-                parraph.className = "post-meta nameParraph";
-                parraph.innerHTML = `Posted by
-                <a id="namePost1" style= "text-decoration: none;" href="#!">Metalligirl</a>
-                on September 24, 2022`;
-                parraph.style.fontSize = "1.125rem";
-                parraph.style.fontStyle = "italic";
-                parraph.style.marginTop = "0";
-                parraph.style.color = "#6c757d";
-                parraph.style.color = "2rem 0";
-
-
-
-                if (a != 0) {
-                    containerPostAll.appendChild(line);
-                    containerPostAll.appendChild(sectionPostAll)
-                    sectionPostAll.appendChild(postTittle);
-                    sectionPostAll.appendChild(subTitle);
-                    sectionPostAll.appendChild(parraph)
-                } else {
-                    containerPostAll.appendChild(sectionPostAll)
-                    sectionPostAll.appendChild(postTittle);
-                    sectionPostAll.appendChild(subTitle);
-                    sectionPostAll.appendChild(parraph)
-                }
-            })
-    }
-}
+// Modal Post Function
 
 function modalPost(i) {
-    mainDisplay.removeChild(container);
 
-    let modalPost = document.createElement("div");
-    modalPost.className = "shadow-lg p-3 mb-5 bg-white rounded";
-    modalPost.style.height = "700px";
+    const fetchReq1 = fetch(`http://localhost:3333/posts`)
+        .then((res) => res.json())
+        .then((posts) => {
+            titleModalPost.innerHTML = posts[i].title;
+            bodyModalPost.innerHTML = posts[i].body;
+        })
 
-    let titleModalPost = document.createElement()
 
-    mainDisplay.appendChild(modalPost);
+    const fetchReq2 = fetch(`http://localhost:3333/users`)
+        .then((res) => res.json())
+        .then((users) => {
+            name.innerHTML = users[i].name;
+            userName.innerHTML = users[i].username;
+            userEmail.innerHTML = users[i].email;
+        });
 
+    const fetchReq3 = fetch(`http://localhost:3333/comments`)
+        .then((res) => res.json())
+        .then((comments) => {
+            // When it done the "Comment step" add the correct value with the API
+        });
+    container.style.display = "none";
+
+    let displayModalPost = document.createElement("div");
+    displayModalPost.className = "shadow-lg p-3 mb-5 bg-white rounded";
+    displayModalPost.style.height = "700px";
+
+    let titleModalPost = document.createElement("h2");
+    titleModalPost.className = 'post-title';
+    titleModalPost.innerHTML = "TITLE"; // Change title
+    displayModalPost.appendChild(titleModalPost);
+
+    let bodyModalPost = document.createElement("p");
+    bodyModalPost.className = 'subTitle post-subtitle';
+    bodyModalPost.innerHTML = "quia et suscipit o" // Change SubsTitle
+    bodyModalPost.style.fontSize = "1.125rem";
+    bodyModalPost.style.fontStyle = "italic";
+    bodyModalPost.style.marginTop = "0";
+    bodyModalPost.style.color = "#6c757d";
+    bodyModalPost.style.color = "2rem 0";
+    displayModalPost.appendChild(bodyModalPost);
+
+    let name = document.createElement('h5');
+    name.className = 'subTitle post-subtitle';
+    name.innerHTML = "Alvaro Alonso"; // Change RealName
+    displayModalPost.appendChild(name);
+
+    let userName = document.createElement('p');
+    userName.className = 'subTitle post-subtitle';
+    userName.innerHTML = "alvaroalonsoDev"; // Change UserName
+    displayModalPost.appendChild(userName);
+
+    let userEmail = document.createElement('p');
+    userEmail.className = 'subTitle post-subtitle';
+    userEmail.innerHTML = "airuritac@gmail.com"; // Change email
+    displayModalPost.appendChild(userEmail);
+
+    let lineModalPost = document.createElement("hr")
+    lineModalPost.className = 'hr-divider';
+    displayModalPost.appendChild(lineModalPost);
+
+    let commentsTitle = document.createElement("h2");
+    commentsTitle.className = 'post-title';
+    commentsTitle.innerHTML = "Comments";
+    displayModalPost.appendChild(commentsTitle);
+
+    let buttonLoadComments = document.createElement("button")
+    buttonLoadComments.className = "btnLC";
+    buttonLoadComments.innerHTML = "Load comments";
+    displayModalPost.appendChild(buttonLoadComments);
+
+    displayModalPost.appendChild(lineModalPost);
+
+    let buttonEdit = document.createElement("button")
+    buttonEdit.className = "btnEdit";
+    buttonEdit.innerHTML = "Edit";
+    displayModalPost.appendChild(buttonEdit);
+
+    let buttonDelete = document.createElement("button")
+    buttonDelete.className = "btnEdit";
+    buttonDelete.innerHTML = "Delete";
+    displayModalPost.appendChild(buttonDelete);
+
+    mainDisplay.appendChild(displayModalPost);
+
+
+
+
+
+
+    // const allData = Promise.all([fetchReq1, fetchReq2, fetchReq3]);
+    // console.log(allData);
+    // fetch('http://localhost:3333/posts'),
+    //     fetch('http://localhost:3333/users'),
+    //     fetch('http://localhost:3333/comments')
+    //         .then(data => data.json())
+    //         .then(posts => {
+    //             console.log(posts);
+    //         })
+    //         .then()
 }
-
