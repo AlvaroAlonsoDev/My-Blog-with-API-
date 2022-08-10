5e45e582f661ec935371cf40453c814940215dec
Vickie Scullard <vickie.scullard@gmail.com>
2022-08-10 15:02:56 +0100

    create all html, edit text content index html

--- js/scripts.js
+++ js/scripts.js
@@ -29,20 +29,22 @@ window.addEventListener('DOMContentLoaded', () => {
 // API
 
 window.onload = fetiche();
+window.onload = feticheAll();
 
 const
     containerPost = document.getElementById("containerPost");
+    containerPostAll = document.getElementById("containerPostAll");
 
 
 
 function fetiche() {
     for (let i = 0; i < 5; i++) {
-        fetch('http://localhost:3333/posts')
+        fetch('http://localhost:3000/posts')
             .then(data => data.json())
             .then(posts => {
 
                 let line = document.createElement("hr")
-                line.className = 'my-4';
+                line.className = 'hr-divider';
 
                 let sectionPost = document.createElement("section")
                 sectionPost.className = "displayPost";
@@ -85,3 +87,52 @@ function fetiche() {
     }
 }
 
+function feticheAll() {
+    for (let a = 0; a < 500; a++) {
+        fetch('http://localhost:3000/posts')
+            .then(data => data.json())
+            .then(posts => {
+
+                let line = document.createElement("hr")
+                line.className = 'hr-divider';
+
+                let sectionPostAll = document.createElement("sectionAll")
+                sectionPostAll.className = "displayPost";
+                sectionPostAll.onclick = () => modalPostAll(); // Add the function to watch the "MODAL POST"
+
+                let postTittle = document.createElement('h2');
+                postTittle.className = 'post-title';
+                postTittle.innerHTML = posts[a].title;
+
+                let subTitle = document.createElement('h3');
+                subTitle.className = 'subTitle post-subtitle';
+                subTitle.innerHTML = posts[a].body;
+
+                let parraph = document.createElement("p");
+                parraph.className = "post-meta nameParraph";
+                parraph.innerHTML = `Posted by
+                <a id="namePost1" style= "text-decoration: none;" href="#!">Metalligirl</a>
+                on September 24, 2022`;
+                parraph.style.fontSize = "1.125rem";
+                parraph.style.fontStyle = "italic";
+                parraph.style.marginTop = "0";
+                parraph.style.color = "#6c757d";
+                parraph.style.color = "2rem 0";
+
+
+
+                if (a != 0) {
+                    containerPostAll.appendChild(line);
+                    containerPostAll.appendChild(sectionPostAll)
+                    sectionPostAll.appendChild(postTittle);
+                    sectionPostAll.appendChild(subTitle);
+                    sectionPostAll.appendChild(parraph)
+                } else {
+                    containerPostAll.appendChild(sectionPostAll)
+                    sectionPostAll.appendChild(postTittle);
+                    sectionPostAll.appendChild(subTitle);
+                    sectionPostAll.appendChild(parraph)
+                }
+            })
+    }
+}
